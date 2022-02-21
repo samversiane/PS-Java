048ff498d084cc56c1cb208492d644c431b2f23d
SamuelVersiane <samuellversiane@gmail.com>
2022-02-20 21:40:56 -0300

    entrypoint, db, dbseed, ProductRepository

--- src/main/java/br/com/supera/game/store/entities/Product.java
+++ src/main/java/br/com/supera/game/store/entities/Product.java
@@ -8,6 +8,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
+@Table(name = "tb_product")
 public class Product {
 
    @Id
---
+++ src/main/resources/data.sql
@@ -0,0 +1,9 @@
+INSERT INTO tb_product (name, price, score, image) VALUES ("Super Mario Odyssey", 197.88, 100, "super-mario-odyssey.png");
+INSERT INTO tb_product (name, price, score, image) VALUES ("Call Of Duty Infinite Warfare", 49.99, 80, "call-of-duty-infinite-warfare.png");
+INSERT INTO tb_product (name, price, score, image) VALUES ("The Witcher III Wild Hunt", 119.5, 250, "the-witcher-iii-wild-hunt.png");
+INSERT INTO tb_product (name, price, score, image) VALUES ("Call Of Duty WWII", 249.99, 205, "call-of-duty-wwii.png");
+INSERT INTO tb_product (name, price, score, image) VALUES ("Mortal Kombat XL", 69.99, 150, "mortal-kombat-xl.png");
+INSERT INTO tb_product (name, price, score, image) VALUES ("Shards of Darkness", 71.94, 400, "shards-of-darkness.png");
+INSERT INTO tb_product (name, price, score, image) VALUES ("Terra Média: Sombras de Mordor", 79.99, 50, "terra-media-sombras-de-mordor.png");
+INSERT INTO tb_product (name, price, score, image) VALUES ("FIFA 18", 195.39, 325, "fifa-18.png");
+INSERT INTO tb_product (name, price, score, image) VALUES ("Horizon Zero Dawn", 115.8, 290, "horizon-zero-dawn.png");
